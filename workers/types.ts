d5cce6d2808267a1afeaa75ca5382e755ee698d2
guiming99 <153085985+guiming99@users.noreply.github.com>
2026-08-25 15:15:10 +0800

    chore: type multi-domain configuration

--- workers/types.ts
+++ workers/types.ts
@@ -1,10 +1,11 @@
 // Copyright (c) 2026 Cloudflare, Inc.
-// Licensed under the Apache 2.0 license found in the LICENSE file or at:
+// Licensed under the Apache 2.0 license found in the LICENSE file and at:
 //     https://opensource.org/licenses/Apache-2.0
 
 export interface Env extends Cloudflare.Env {
 	POLICY_AUD: string;
 	TEAM_DOMAIN: string;
+	TEAM_DOMAINS?: string;
 	APP_NAME?: string;
 	ADMIN_EMAIL?: string;
 	ADMIN_PASSWORD?: string;
